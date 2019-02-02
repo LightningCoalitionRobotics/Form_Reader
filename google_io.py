22e40f77b281edadbb9ca05662a75fccb1bd05b7
YoinkerDoinker <chris_cross_country@yahoo.com>
2019-02-02 11:44:32 -0500

    Fixed bullet points.

--- google_io.py
+++ google_io.py
@@ -2,7 +2,7 @@ import OpenSSL
 import json
 import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
-from pylatex import Document, Section, Subsection, Command
+from pylatex import Document, Section, Subsection, Command, Itemize
 import pylatex as pyl
 
 json_key = json.load(open('creds.json'))
@@ -27,16 +27,16 @@ improve_par = {}
 for i in range(2, 5):
     if sheet.acell('A{}'.format(i)).value != "Stop":
         v = i-1
-        date[v] = sheet.cell(i, 3)
-        author[v] = sheet.cell(i, 4)
-        wanted_bul[v] = sheet.cell(i, 5)
-        wanted_par[v] = sheet.cell(i, 6)
-        accomplished_bul[v] = sheet.cell(i, 7)
-        accomplished_par[v] = sheet.cell(i, 8)
-        didnt_do_bul[v] = sheet.cell(i, 9)
-        improve_par[v] = sheet.cell(i, 10)
-        next_bul[v] = sheet.cell(i, 11)
-        next_par[v] = sheet.cell(i , 12)
+        date[v] = sheet.cell(i, 3).value
+        author[v] = sheet.cell(i, 4).value
+        wanted_bul[v] = sheet.cell(i, 5).value.split(',')
+        wanted_par[v] = sheet.cell(i, 6).value
+        accomplished_bul[v] = sheet.cell(i, 7).value.split(',')
+        accomplished_par[v] = sheet.cell(i, 8).value
+        didnt_do_bul[v] = sheet.cell(i, 9).value.split(',')
+        improve_par[v] = sheet.cell(i, 10).value
+        next_bul[v] = sheet.cell(i, 11).value.split(',')
+        next_par[v] = sheet.cell(i , 12).value
 
         print("Finished parsing row {}".format(v))
 
@@ -47,7 +47,7 @@ for i in range(2, 5):
 Latex Section Begins:
 '''
 
-for i in range(2, 5):
+for i in range(2, 3):
     if __name__ == '__main__':
         v = i - 1
 
@@ -59,37 +59,37 @@ for i in range(2, 5):
 
         doc.preamble.append(pyl.Command('pagestyle', 'fancy'))
         doc.preamble.append(pyl.Command('title', 'Notebook'))
-        doc.append(date[v] + " - " + author[v]) #or preamble.append?
+        doc.append(date[v] + " - " + author[v])
 
         with doc.create(Section('Our Plan')):
             # remove [] from all strings
             with doc.create(Itemize()) as itemize:
                 for v in wanted_bul:
-                    itemize.add_item(wanted_bul[v])
+                    for item in wanted_bul[v]:
+                        itemize.add_item(item)
                 itemize.append(Command("ldots"))
             doc.append(wanted_par[v])
         with doc.create(Section('What We Got Done')):
             with doc.create(Itemize()) as itemize:
                 for v in accomplished_bul:
-                    itemize.add_item(accomplished_bul[v])
+                    for item in wanted_bul[v]:
+                        itemize.add_item(item)
                 itemize.append(Command("ldots"))
             doc.append(accomplished_par[v])
         with doc.create(Section('What We Can Improve On For Next Time')):
-            with doc.create(Itemize()) as itemize:
-                for v in improve_bul:
-                    itemize.add_item(improve_bul[v])
-                itemize.append(Command("ldots"))
             doc.append(improve_par[v])
+            with doc.create(Itemize()) as itemize:
+                for v in didnt_do_bul:
+                    for item in wanted_bul[v]:
+                        itemize.add_item(item)
+                itemize.append(Command("ldots"))
         with doc.create(Section('Next Practice')):
             with doc.create(Itemize()) as itemize:
                 for v in next_bul:
-                    itemize.add_item(next_bul[v])
+                    for item in wanted_bul[v]:
+                        itemize.add_item(item)
                 itemize.append(Command("ldots"))
             doc.append(next_par[v])
         doc.generate_pdf("notebook{}".format(v), clean_tex=False)
 
-
-
 #     #cd C:\Users\chris\Desktop\Coding\Python\Form Reader
-#
-#     # redo form with author
